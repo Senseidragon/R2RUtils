898962220c310b56c2909426db7b3f402c2a6997
Senseidragon <senseidragon@gmail.com>
2017-02-14 03:27:32 -0500

    Initial 1.9.4 commit

---
+++ src/main/java/com/senseidragon/r2rutil/proxy/ServerProxy.java
@@ -0,0 +1,18 @@
+/*
+ * R2RUtils - ServerProxy
+ *
+ *  Copyright (c) 2015. Jesse Patterson ("Senseidragon")
+ *
+ *  @author Senseidragon
+ *  @license Lesser GNU Public License v3 (http://www.gnu.org/licensese/lgpl.html)
+ */
+
+package com.senseidragon.r2rutil.proxy;
+
+// Yeah, like the server is going to render anything.
+public class ServerProxy extends CommonProxy {
+    @Override
+    public void registerRenderers() {
+
+    }
+}
